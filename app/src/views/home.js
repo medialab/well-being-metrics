8b459d91b06212de017788b304196c8646dafec1
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-06-16 10:17:16 +0200

    Add angular-translate for localization

--- app/src/views/home.js
+++ app/src/views/home.js
@@ -2,13 +2,25 @@
 
 angular.module('app.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(function($routeProvider, $translateProvider) {
   $routeProvider.when('/', {
     templateUrl: 'src/views/home.html'
   , controller: 'HomeController'
   })
-}])
+
+  $translateProvider.translations('en', {
+    'TITLE': 'Hello',
+    'FOO': 'This is a paragraph'
+  });
+ 
+  $translateProvider.translations('de', {
+    'TITLE': 'Hallo',
+    'FOO': 'Dies ist ein Absatz'
+  });
+ 
+  $translateProvider.preferredLanguage('en');
+})
 
 .controller('HomeController', function($scope, $location) {
-
+	
 });
