59e3ae82eda15955fcf39d50a3249f2b74053c57
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-06-16 12:20:59 +0200

    locale loaded from files

--- app/src/views/home.js
+++ app/src/views/home.js
@@ -2,30 +2,14 @@
 
 angular.module('app.home', ['ngRoute'])
 
-.config(function($routeProvider, $translateProvider) {
+.config(function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'src/views/home.html'
   , controller: 'HomeController'
   })
-
-  var translationEn = {
-    'TITLE': 'Hello',
-    'FOO': 'This is a paragraph'
-  }
-
-  $translateProvider.translations('en', translationEn);
- 
-  $translateProvider.translations('de', {
-    'TITLE': 'Hallo',
-    'FOO': 'Dies ist ein Absatz'
-  });
- 
-  $translateProvider
-  	.preferredLanguage('en')
-  	// .determinePreferredLanguage()
-  	.fallbackLanguage('en');
 })
 
-.controller('HomeController', function($scope, $location) {
-	
+.controller('HomeController', function($scope, $location, $translate, $translatePartialLoader) {
+  $translatePartialLoader.addPart('home');
+  $translate.refresh();
 });
