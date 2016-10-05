aa22c348f89326d0e7832b6709bd341b9b919e89
Davy Peter Braun <hello@davybraun.com>
2016-10-05 18:45:06 +0200

    Set snake to camel case in copy/pasta.

--- app/src/directives.js
+++ app/src/directives.js
@@ -1489,11 +1489,11 @@ angular.module('app.directives', [])
               return Promise.resolve();
           }
 
-          var start_time = Date.now();
-          var end_time = start_time + duration;
+          var startTime = Date.now();
+          var endTime = startTime + duration;
 
-          var start_top = element.scrollTop;
-          var distance = target - start_top;
+          var startTop = element.scrollTop;
+          var distance = target - startTop;
 
           // based on http://en.wikipedia.org/wiki/Smoothstep
           var smooth_step = function(start, end, point) {
@@ -1517,12 +1517,12 @@ angular.module('app.directives', [])
 
                   // set the scrollTop for this frame
                   var now = Date.now();
-                  var point = smooth_step(start_time, end_time, now);
-                  var frameTop = Math.round(start_top + (distance * point));
+                  var point = smooth_step(startTime, endTime, now);
+                  var frameTop = Math.round(startTop + (distance * point));
                   element.scrollTop = frameTop;
 
                   // check if we're done!
-                  if(now >= end_time) {
+                  if(now >= endTime) {
                       resolve();
                       return;
                   }
